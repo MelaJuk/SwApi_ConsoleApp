12d775a2a2ad284420947994967fecf8bb830e0c
MelaJuk <84007457+MelaJuk@users.noreply.github.com>
2021-09-30 15:37:08 +0200

    Ajoutez des fichiers projet.

---
+++ SwApi_ConsoleApp/People.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SwApi_ConsoleApp
+{
+    
+    public class People
+    {
+        private string name { get; set; }
+        private string height { get; set; }
+        private string mass { get; set; }
+        private string hair_color { get; set; }
+        private string skin_color { get; set; }
+        private string eye_color { get; set; }
+        private string birth_year { get; set; }
+        private string gender { get; set; }
+        private string homeworld { get; set; }
+        private string[] films { get; set; }
+        private object[] species { get; set; }
+        private string[] vehicles { get; set; }
+        private string[] starships { get; set; }
+        private DateTime created { get; set; }
+        private DateTime edited { get; set; }
+        private string url { get; set; }
+
+        public People()
+        {
+
+        }
+    }
+
+
+   
+}
---
+++ SwApi_ConsoleApp/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Net.Http.Json;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json;
+
+namespace SwApi_ConsoleApp
+{
+    class Program
+    {
+        static readonly HttpClient client = new HttpClient();
+        private const String requestUrlPeople = "https://swapi.dev/api/people/10/";
+
+
+        static async Task Main()
+        {
+            try
+            {
+                // get people name and display it
+                string responseBodyPeople = await client.GetStringAsync(requestUrlPeople);
+                JObject jsonPeople = JObject.Parse(responseBodyPeople);
+                Console.WriteLine(jsonPeople.GetValue("name"));
+
+                // get vehicle'url 
+                string requestUrlVehicle = jsonPeople.GetValue("vehicles").ToString();
+
+                string[] charsToRemove = new string[] { "[", "]", " ", "\"" };
+                foreach (var c in charsToRemove)
+                {
+                    requestUrlVehicle = requestUrlVehicle.Replace(c, string.Empty);
+                }
+
+                // display name of the vehicule 
+                string responseBodyVehicles = await client.GetStringAsync(requestUrlVehicle);
+                JObject jsonVehicles = JObject.Parse(responseBodyVehicles);
+                Console.WriteLine(jsonVehicles.GetValue("name"));
+            }
+            catch (HttpRequestException e)
+            {
+                Console.WriteLine("Exception caught!");
+                Console.WriteLine("Message :{0} ", e.Message);
+            }
+        }
+    }
+}
